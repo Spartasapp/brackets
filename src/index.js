f70fc6e0bb7d728d0443aa4fc0a497ad8de43644
Spartasapp <spartateambuilder@mail.ru>
2019-03-23 13:42:21 +0300

    feat: Implement function which check brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let ar = str.split('');
+let counter = 0;
+let stack = [];
+ar.forEach(function (element) {
+    // count open brackets == closed brackets
+    for(let i = 0; i<bracketsConfig.length; i++){
+        if (counter <0){
+            break;
+        }
+        for(let k = 0; k<bracketsConfig[i].length; k++){
+            if (element == bracketsConfig[i][0] && element == bracketsConfig[i][1] && element == stack[stack.length - 1]) {
+                stack.pop();
+                return counter--;
+            }
+            else if (bracketsConfig[i][1] == element && bracketsConfig[i][0] == stack[stack.length-1]) {
+                stack.pop();
+                return counter--;
+            } 
+            else if (k==0 && bracketsConfig[i][k] == element ) {
+               stack.push(element);
+               return counter++;
+            } 
+            else if( bracketsConfig[i][1] == element && stack[stack.length - 1] == bracketsConfig[i][0]) { 
+                stack.pop();
+               return counter--; 
+            }
+            else if (bracketsConfig[i][1] == element){
+                 counter--;
+                 if (counter <0){
+                    break;
+                }
+            }
+        }
+        
+    }
+    
+});
+if (counter <0){
+    return false;
+}
+else if (counter > 0){
+    return false;
+}
+else if (counter == 0 && stack.length == 0) {
+    return true;
+}
 }
